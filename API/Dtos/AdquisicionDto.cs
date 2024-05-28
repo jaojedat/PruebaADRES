8ab9a58e3ab09969fdf049ccb1eb1b86e37e5451
jaojedat <jaojedat95@gmail.com>
2024-05-27 23:22:30 -0500

    Corrigiendo Valor Total

--- API/Dtos/AdquisicionDto.cs
+++ API/Dtos/AdquisicionDto.cs
@@ -13,6 +13,7 @@ namespace API.Dtos
         public string TipoBienServicio { get; set; }
         public int Cantidad { get; set; }
         public decimal ValorUnitario { get; set; }
+        public decimal ValorTotal => Cantidad * ValorUnitario;
         public DateTime FechaAdquisicion { get; set; }
         public string Proveedor { get; set; }
         public string Documentacion { get; set; }
